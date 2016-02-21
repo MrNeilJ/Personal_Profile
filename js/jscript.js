80686fe2fcb3298343db6e39038547e11020bae2
Neil Johnson <Mr.Neil.J@gmail.com>
2016-02-21 12:39:31 -0800

    Further Updates to allow mobile device functionality
    Added in meta tags to account for pixel density so that I can keep an
    accurate reading on the size of the screen so that it fits all device
    responsively

--- js/jscript.js
+++ js/jscript.js
@@ -1,16 +1,29 @@
 	<!--Script to run JQuery SmoothScroll-->
-	$(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
+	$(function () {
+		$('a[href*="#"]:not([href="#"])').click(function () {
+			if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+				if (target.length) {
+					$('html, body').animate({
+						scrollTop: target.offset().top
+					}, 1000);
+					return false;
+				}
+			}
+		});
+	});
 	<!--End of SmoothScroll Script-->
+
+	$(document).ready(function () {
+		$("div#mobile-ul").hide();
+		$("li#menu").click(function () {
+			$("div#mobile-ul").slideToggle("slow");
+		});
+		$("a.mobile-link").click(function () {
+			$("div#mobile-ul").slideUp("slow");
+		});
+
+	});
+
+	<!--End of Mobile Toggle-->
